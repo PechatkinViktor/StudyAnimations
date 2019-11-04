ab6e6edbc1b31252a37bc484628f75e3faeded6b
Viktor <pechatkinvictor@gmail.com>
2019-11-04 18:02:57 +0300

    + анимация FrameToFrame

---
+++ app/src/main/java/com/pechatkin/sbt/studyanimations/FrameToFrame.java
@@ -0,0 +1,40 @@
+package com.pechatkin.sbt.studyanimations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FrameToFrame extends AppCompatActivity {
+
+    private ImageView mHorseImageView;
+    private AnimationDrawable mDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_to_frame);
+
+        init();
+
+        mHorseImageView.post(() -> mDrawable.start());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDrawable.start();
+    }
+
+    @Override
+    protected void onStop() {
+        mDrawable.stop();
+        super.onStop();
+    }
+
+    private void init() {
+        mHorseImageView = findViewById(R.id.horseImageView);
+        mDrawable = (AnimationDrawable) mHorseImageView.getDrawable();
+    }
+}
--- app/src/main/java/com/pechatkin/sbt/studyanimations/MainActivity.java
+++ app/src/main/java/com/pechatkin/sbt/studyanimations/MainActivity.java
@@ -2,6 +2,8 @@ package com.pechatkin.sbt.studyanimations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        onClickGoTo(R.id.animationFrameToFrame, FrameToFrame.class);
+    }
+
+    private void onClickGoTo(int viewId, Class<? extends Activity> whereToGo) {
+        findViewById(viewId).setOnClickListener((v) ->
+                startActivity(new Intent(this, whereToGo))
+        );
     }
 }
